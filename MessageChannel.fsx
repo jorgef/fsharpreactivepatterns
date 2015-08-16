66e8ee3a01bad0ee86e1e9519658a30699c85f59
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-08-17 08:42:30 +1000

    initial commit

---
+++ MessageChannel.fsx
@@ -0,0 +1,18 @@
+﻿#r @"packages\Akka.1.0.4\lib\net45\Akka.dll"
+#r @"packages\Akka.FSharp.1.0.4\lib\net45\Akka.FSharp.dll"
+
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+let messageChannel (mailbox: Actor<_>) = 
+    let rec loop () = actor {
+        let! message = mailbox.Receive ()
+        printfn "%s" message
+        return! loop ()
+    }
+    loop ()
+
+let messageChannelActor = spawn system "message-channel" messageChannel
+
+messageChannelActor <! "hello"
