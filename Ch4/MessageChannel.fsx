3b0d7fc9a4d3d55419859b2f2acfdb11e63178d2
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-09-01 08:23:09 +1000

    Message channel pattern

--- Ch4/MessageChannel.fsx
+++ Ch4/MessageChannel.fsx
@@ -7,16 +7,18 @@
 
 open Akka.FSharp
 
+type ProcessorMessage = ProcessJob of int * int * int
+
 let system = System.create "system" <| Configuration.load ()
 
-let messageChannel (mailbox: Actor<_>) = 
+let processorActor (mailbox: Actor<_>) = 
     let rec loop () = actor {
-        let! message = mailbox.Receive ()
-        printfn "Received: %s" message
+        let! ProcessJob(x,y,z) = mailbox.Receive ()
+        printfn "Received: %i %i %i" x y z
         return! loop ()
     }
     loop ()
 
-let messageChannelActor = spawn system "message-channel" messageChannel
+let processor = spawn system "processor" processorActor
 
-messageChannelActor <! "hello"
+processor <! ProcessJob(1, 3, 5)
