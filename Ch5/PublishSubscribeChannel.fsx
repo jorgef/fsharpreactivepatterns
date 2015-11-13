dd24fb223cf4e409bd2e4154cd324dd74543264d
Jorge <jorge.fioranelli@gmail.com>
2015-11-13 08:39:44 -0500

    Added publish-subscribe channel

---
+++ Ch5/PublishSubscribeChannel.fsx
@@ -0,0 +1,22 @@
+﻿#load "..\References.fsx"
+
+open Akka.FSharp
+
+let system = System.create "TradingSystem" <| Configuration.load ()
+
+type Money = Money of amount: decimal
+type Market = Market of name: string
+type Symbol = Symbol of name: string
+type PricedQuoted = { Market: Market; Ticker: Symbol; Price: Money }
+
+let quoteListener (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! { Market = Market m; Ticker = Symbol t; Price = Money p } = mailbox.Receive ()
+        printfn "QuoteListener: PricedQuoted received, market: %s, ticker: %s, price: %M" m t p 
+        return! loop ()
+    }
+    loop () 
+
+let quoteListenerRef = spawn system "quoteListenerRef" quoteListener
+subscribe typeof<PricedQuoted> quoteListenerRef system.EventStream
+publish { Market = Market("quotes/NASDAQ"); Ticker = Symbol "MSFT"; Price = Money(37.16m) } system.EventStream
