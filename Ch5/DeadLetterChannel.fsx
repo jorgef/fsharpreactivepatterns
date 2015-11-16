f61c347bd4b7a9273d979d2a5c32e480d119bfce
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-11-15 21:30:23 -0500

    Added dead letter channel

---
+++ Ch5/DeadLetterChannel.fsx
@@ -0,0 +1,20 @@
+﻿#load "..\References.fsx"
+
+open Akka.Event
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+let sysListener (mailbox: Actor<DeadLetter>) = 
+    let rec loop () = actor {
+        let! deadLetter = mailbox.Receive ()
+        printfn "SysListner, DeadLetter received: %A" deadLetter.Message
+        return! loop ()
+    }
+    loop ()
+
+let sysListenerRef = spawn system "sysListener" sysListener
+subscribe typeof<DeadLetter> sysListenerRef system.EventStream
+
+let deadActorRef = select "akka://system/user/deadActor" system
+deadActorRef <! "Message to dead actor"
