d6d346f5a467b04d311550627ff00ee6611b8e7b
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-11-15 20:40:11 -0500

    Added invalid message channel

---
+++ Ch5/InvalidMessageChannel.fsx
@@ -0,0 +1,52 @@
+﻿#load "..\References.fsx"
+
+open System
+open System.Text
+open Akka.Actor
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+type InvalidMessage<'a> = {
+    Sender: IActorRef
+    Receiver: IActorRef
+    Message: 'a
+}
+
+let invalidMessageChannel (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! { Sender = s; Receiver = r; Message = m } = mailbox.Receive ()
+        printfn "InvalidMessageChannel: InvalidMessage received, message: %A" m
+        return! loop ()
+    }
+    loop ()
+
+type ProcessIncomingOrder = ProcessIncomingOrder of byte array
+
+let authenticator (nextFilter: IActorRef) (invalidMessageChannel: IActorRef) (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! message = mailbox.Receive ()
+        match box message with
+        | :? ProcessIncomingOrder as message ->
+            let (ProcessIncomingOrder(bytes)) = message 
+            let text = Encoding.Default.GetString bytes
+            printfn "Decrypter: processing %s" text
+            let orderText = text.Replace ("(encryption)", String.Empty)
+            nextFilter <! ProcessIncomingOrder(Encoding.Default.GetBytes orderText)
+        | invalid -> invalidMessageChannel <! { Sender = mailbox.Sender (); Receiver = mailbox.Self; Message = invalid }
+        return! loop () 
+    }
+    loop ()
+
+let nextFilter (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! message = mailbox.Receive ()
+        return! loop ()
+    }
+    loop ()
+
+let invalidMessageChannelRef = spawn system "invalidMessageChannel" invalidMessageChannel
+let nextFilterRef = spawn system "nextFilter" nextFilter
+let authenticatorRef = spawn system "authenticator" <| authenticator nextFilterRef invalidMessageChannelRef
+
+authenticatorRef <! "Invalid message"
