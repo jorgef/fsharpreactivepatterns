9d59d8199d4bc70c3a144825f0c0e92e9e9e60c6
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-11-28 13:27:50 -0500

    Added message bridge

---
+++ Ch5/MessageBridge.fsx
@@ -0,0 +1,26 @@
+﻿#load "..\References.fsx"
+
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+type RabbitMQTextMessage = RabbitMQTextMessage of string
+
+let inventoryProductAllocationBridge (mailbox: Actor<_>) =
+    let translatedToInventoryProductAlloction = sprintf "Inventory product alloction for %s"
+    let acknowledgeDelivery (RabbitMQTextMessage textMessage) = printfn "InventoryProductAllocationBridge: acknowledged '%s'" textMessage
+
+    let rec loop () = actor {
+        let! message = mailbox.Receive ()
+        let (RabbitMQTextMessage textMessage) = message
+        printfn "InventoryProductAllocationBridge: received '%s'" textMessage
+        let inventoryProductAllocation = translatedToInventoryProductAlloction textMessage
+        printfn "InventoryProductAllocationBridge: translated '%s'" inventoryProductAllocation
+        acknowledgeDelivery message
+        return! loop ()
+    }
+    loop ()
+
+let inventoryProductAllocationBridgeRef = spawn system "inventoryProductAllocationBridge" inventoryProductAllocationBridge
+
+inventoryProductAllocationBridgeRef <! RabbitMQTextMessage "Rabbit test message"
