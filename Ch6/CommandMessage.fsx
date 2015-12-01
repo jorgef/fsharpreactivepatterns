36246e27226cea35b4681478737f90e7867198ab
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-12-01 08:30:50 -0500

    Added command message

---
+++ Ch6/CommandMessage.fsx
@@ -0,0 +1,28 @@
+﻿#load "..\References.fsx"
+
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+type Money = Money of decimal
+
+type TradingCommand =
+    | ExecuteBuyOrder of portfolioId: string * symbol: string * quantity: int * price: Money
+    | ExecuteSellOrder of portfolioId: string * symbol: string * quantity: int * price: Money
+
+let stockTrader (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! message = mailbox.Receive ()
+        match message with
+        | ExecuteBuyOrder(portfolioId, symbol, quantity, price) as buy -> 
+            printfn "StockTrader: buying for: %A" buy
+        | ExecuteSellOrder(portfolioId, symbol, quantity, price) as sell ->
+            printfn "StockTrader: selling for: %A" sell
+        return! loop () 
+    }
+    loop ()
+
+let stockTraderRef = spawn system "stockTrader" <| stockTrader
+
+stockTraderRef <! ExecuteBuyOrder("p123", "MSFT", 100, Money 31.85m)
+stockTraderRef <! ExecuteSellOrder("p456", "MSFT", 200, Money 31.80m)
