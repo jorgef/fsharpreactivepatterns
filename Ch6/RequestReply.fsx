ab620972d9ece4816c69a08166635d0ca35ca633
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-12-01 21:53:32 -0500

    Added request-reply

---
+++ Ch6/RequestReply.fsx
@@ -0,0 +1,38 @@
+﻿#load "..\References.fsx"
+
+open Akka.Actor
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+type ServerMessage = Request of string
+type ClientMessage =
+    | Reply of string
+    | StartWith of IActorRef
+
+let client (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! message = mailbox.Receive ()
+        match message with
+        | StartWith server ->
+            printfn "Client: is starting..."
+            server <! Request "REQ-1"
+        | Reply what ->
+            printfn "Client: received response: %s" what
+        return! loop ()
+    }
+    loop ()
+
+let server (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! Request what = mailbox.Receive ()
+        printfn "Server: received request value: %s" what
+        mailbox.Sender () <! Reply (sprintf "RESP-1 for %s" what)
+        return! loop ()
+    }
+    loop ()
+
+let clientRef = spawn system "client" client
+let serverRef = spawn system "server" server
+
+clientRef <! StartWith serverRef
