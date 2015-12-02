b3a519d9f23c9bd18977019b99fa90a3a80de130
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-12-01 21:38:13 -0500

    Added event message

---
+++ Ch6/EventMessage.fsx
@@ -0,0 +1,34 @@
+﻿
+#load "..\References.fsx"
+
+open Akka.Actor
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+type Money = Money of decimal
+type PriceQuote = { QuoterId: string; RetailerId: string; RfqId: string; ItemId: string; RetailPrice: decimal; DiscountPrice: decimal }
+type RequestPriceQuote = RequestPriceQuote of rfqId: string * itemId: string * retailPrice: Money * orderTotalRetailPrice: Money
+type PriceQuoteFulfilled = PriceQuoteFulfilled of priceQuote: PriceQuote 
+
+let quotation subscriber (mailbox: Actor<_>) =
+    let quoterId = mailbox.Self.Path.Name
+    let rec loop () = actor {
+        let! RequestPriceQuote(rfqId, itemId, Money retailPrice, _) = mailbox.Receive ()
+        subscriber <! PriceQuoteFulfilled { QuoterId = quoterId; RetailerId = "Retailer1"; RfqId = rfqId; ItemId = itemId; RetailPrice = retailPrice; DiscountPrice = retailPrice * 0.90m }
+        return! loop ()
+    }
+    loop ()
+
+let subscriber (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! message = mailbox.Receive ()
+        printfn "Requester: event %A" message
+        return! loop ()
+    }
+    loop ()
+
+let subscriberRef = spawn system "subscriber" subscriber
+let quotationRef = spawn system "quotation" <| quotation subscriberRef
+
+quotationRef <! RequestPriceQuote("1", "1", Money 10m, Money 10m)
