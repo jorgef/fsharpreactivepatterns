35ab05480d03c3d208be51dcf9143ef4d1da0417
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-12-03 18:13:56 -0500

    Added format indicator

---
+++ Ch6/FormatIndicator.fsx
@@ -0,0 +1,26 @@
+﻿#load "..\References.fsx"
+
+open System
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+type Money = Money of decimal
+type ExecuteBuyOrder = { PortfolioId: string; Symbol: string; Quantity: int; Price: Money; DateTimeOrdered: DateTimeOffset option; Version: int } with
+    static member ``create v1`` portfolioId symbol quantity price = { PortfolioId = portfolioId; Symbol = symbol; Quantity = quantity; Price = price; DateTimeOrdered = None; Version = 1 } 
+    static member ``create v2`` portfolioId symbol quantity price = { PortfolioId = portfolioId; Symbol = symbol; Quantity = quantity; Price = price; DateTimeOrdered = Some DateTimeOffset.UtcNow; Version = 2 } 
+
+let stockTrader (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! message = mailbox.Receive ()
+        let orderExecutionStartedOn = match message.Version with
+                                      | 1 -> Some DateTimeOffset.UtcNow
+                                      | _ -> message.DateTimeOrdered
+        printfn "StockTrader: orderExecutionStartedOn: %A" orderExecutionStartedOn
+        return! loop () 
+    }
+    loop ()
+
+let stockTraderRef = spawn system "stockTrader" stockTrader
+stockTraderRef <! ExecuteBuyOrder.``create v1`` "1" "11" 10 (Money 50.00m)
+stockTraderRef <! ExecuteBuyOrder.``create v2`` "1" "11" 10 (Money 50.00m)
