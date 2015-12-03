42612c71e6c3e88af45c422881bc26d9ea9efcd3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-12-03 15:00:11 +0100

    Without release notes

--- build.fsx
+++ build.fsx
@@ -108,9 +108,6 @@ let generateHelp fail =
     generateHelp' fail false
 
 Target "GenerateHelp" (fun _ ->
-    DeleteFile "docs/content/release-notes.md"
-    CopyFile "docs/content/" "RELEASE_NOTES.md"
-    Rename "docs/content/release-notes.md" "docs/content/RELEASE_NOTES.md"
 
     DeleteFile "docs/content/license.md"
     CopyFile "docs/content/" "LICENSE.txt"
@@ -120,9 +117,6 @@ Target "GenerateHelp" (fun _ ->
 )
 
 Target "GenerateHelpDebug" (fun _ ->
-    DeleteFile "docs/content/release-notes.md"
-    CopyFile "docs/content/" "RELEASE_NOTES.md"
-    Rename "docs/content/release-notes.md" "docs/content/RELEASE_NOTES.md"
 
     DeleteFile "docs/content/license.md"
     CopyFile "docs/content/" "LICENSE.txt"
