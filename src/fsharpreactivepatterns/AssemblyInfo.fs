b58b4140701a2041463a039b013b3fb3ad09c717
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-12-03 14:06:50 +0100

    Initial commit

---
+++ src/fsharpreactivepatterns/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("fsharpreactivepatterns")>]
+[<assembly: AssemblyProductAttribute("fsharpreactivepatterns")>]
+[<assembly: AssemblyDescriptionAttribute("F# reactive patterns")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
