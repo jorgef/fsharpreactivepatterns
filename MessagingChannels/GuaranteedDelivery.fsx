d1421f4c78bc5eef2abde888748183060bd035a3
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-12-06 20:53:50 -0500

    Added TBD message for persistent actors

--- MessagingChannels/GuaranteedDelivery.fsx
+++ MessagingChannels/GuaranteedDelivery.fsx
@@ -1,16 +1,1 @@
-﻿#load "..\References.fsx"
-
-open Akka.Actor
-open Akka.FSharp
-open Akka.Persistence.FSharp
-
-let system = System.create "system" <| Configuration.load ()
-
-let loanBroker (mailbox: Actor<_>) =
-    let rec loop () = actor {
-        return! loop ()
-    }
-    loop ()
-
-// TDB: Use spawnPersist when is ready (@horusiath is working on it)
-// https://github.com/Horusiath/Akkling/blob/actor-expression-2.0/examples/persistence.fsx
+﻿// TBD: it needs persistence
