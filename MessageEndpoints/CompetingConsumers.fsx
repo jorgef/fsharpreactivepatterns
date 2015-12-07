a3572f1b20df23a53afe0da9133ef9c16d326cbe
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-12-06 22:08:22 -0500

    Added polling consumer and competing consumers

---
+++ MessageEndpoints/CompetingConsumers.fsx
@@ -0,0 +1,20 @@
+﻿#load "..\References.fsx"
+
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+type WorkItem = { Name: string }
+
+let workConsumer (mailbox: Actor<_>) =
+    let rec loop () = actor {
+        let! workItem = mailbox.Receive ()
+        printfn "%s for: %s" mailbox.Self.Path.Name workItem.Name
+        return! loop ()
+    }
+    loop ()
+
+let workItemsProviderRef = spawnOpt system "workItemsProvider" workConsumer [ Router(Akka.Routing.SmallestMailboxPool(5)) ]
+
+[ 1 .. 100 ]
+|> List.iter (fun itemCount -> workItemsProviderRef <! { Name = "WorkItem" + itemCount.ToString () })
---
+++ MessageEndpoints/PollingConsumer.fsx
@@ -0,0 +1,65 @@
+﻿#load "..\References.fsx"
+
+open System
+open Akka.Actor
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+type WorkItem = { Name: string }
+
+type WorkConsumerMessage = 
+    | WorkNeeded
+    | WorkItemsAllocated of workItems: WorkItem list
+    | WorkOnItem of workItem: WorkItem
+
+type WorkItemsProvider = 
+    | AllocateWorkItems of numberOfItems: int
+
+let workItemsProvider (mailbox: Actor<_>) =
+    let rec loop workItemsNamed = actor {
+        let allocateWorkItems numberOfItems =
+            let allocatedWorkItems = 
+                [ 1 .. numberOfItems ]
+                |> List.map (fun itemCount -> 
+                    let nameIndex = workItemsNamed + itemCount
+                    { Name = "WorkItem" + nameIndex.ToString () })
+            allocatedWorkItems, workItemsNamed + numberOfItems
+
+        let! AllocateWorkItems numberOfItems = mailbox.Receive ()
+        let allocatedWorkItems, workItemsNamed = allocateWorkItems(numberOfItems)
+        mailbox.Sender () <! WorkItemsAllocated allocatedWorkItems
+        return! loop workItemsNamed
+    }
+    loop 0
+
+let workConsumer workItemsProvider (mailbox: Actor<_>) =
+    mailbox.Defer (fun () -> mailbox.Context.Stop(workItemsProvider))
+    let rec loop totalItemsWorkedOn = actor {
+        let performWorkOn workItem =
+            let totalItemsWorkedOn = totalItemsWorkedOn + 1
+            if (totalItemsWorkedOn >= 15) then  mailbox.Context.Stop mailbox.Self else ()
+            totalItemsWorkedOn
+
+        let! message = mailbox.Receive ()
+        match message with
+        | WorkItemsAllocated workitems ->
+            printfn "WorkItemsAllocated..."
+            workitems |> List.iter (fun workItem -> mailbox.Self <! WorkOnItem(workItem))
+            mailbox.Self <! WorkNeeded
+            return! loop totalItemsWorkedOn
+        | WorkNeeded ->
+            printfn "WoorkNeeded..."
+            workItemsProvider <! AllocateWorkItems 5
+            return! loop totalItemsWorkedOn
+        | WorkOnItem workItem ->
+            printfn "Performed work on: %s" workItem.Name
+            let totalItemsWorkedOn = performWorkOn workItem
+            return! loop totalItemsWorkedOn
+    }
+    loop 0
+
+let workItemsProviderRef = spawn system "workItemsProvider" workItemsProvider
+let workConsumerRef = spawn system "workConsumer" <| workConsumer workItemsProviderRef
+
+workConsumerRef <! WorkNeeded
