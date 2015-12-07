ad47c28dbcd44a44c3cf78f8ff8f8ffa09270f95
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-12-06 19:02:24 -0500

    Added messaging mapper

---
+++ MessageEndpoints/MessagingMapper.fsx
@@ -0,0 +1,45 @@
+﻿#load "..\References.fsx"
+
+open System
+open Akka.Actor
+open Akka.FSharp
+open Newtonsoft.Json
+
+let system = System.create "system" <| Configuration.load ()
+
+type QueryMonthlyOrdersFor = QueryMonthlyOrdersFor of customerId: string
+type ReallyBigQueryResult = ReallyBigQueryResult of messageBody: string
+
+type IMessageSerializer =
+    abstract member Serialize: obj -> string
+    abstract member Deserialize: string -> 'a
+
+let serializer = { 
+    new IMessageSerializer with
+        member this.Serialize obj = JsonConvert.SerializeObject obj
+        member this.Deserialize json = JsonConvert.DeserializeObject<'a>(json) 
+    }
+        
+let orderQueryService (serializer: IMessageSerializer) (mailbox: Actor<_>) =
+    let monthlyOrdersFor customerId = [ for i in [1 .. 10] -> sprintf "Order data %i" i ]
+    let rec loop () = actor {
+        let! QueryMonthlyOrdersFor(customerId) as message = mailbox.Receive ()
+        printfn "OrderQueryService: Received %A" message
+        let queryResult = monthlyOrdersFor customerId
+        let messageBody = serializer.Serialize(queryResult)
+        mailbox.Sender () <! ReallyBigQueryResult messageBody
+        return! loop ()
+    }
+    loop ()
+
+let caller orderQueryService (mailbox: Actor<_>) =
+    orderQueryService <! QueryMonthlyOrdersFor "123"
+    let rec loop () = actor {
+        let! message = mailbox.Receive ()
+        printfn "Caller: result received: %A" message
+        return! loop ()
+    }
+    loop ()
+
+let orderQueryServiceRef = spawn system "orderQueryService" <| orderQueryService serializer
+let callerRef = spawn system "caller" <| caller orderQueryServiceRef
