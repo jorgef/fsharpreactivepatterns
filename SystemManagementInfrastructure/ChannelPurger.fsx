6a8576d4cce354cd278e9deccfc2350aaffb47f3
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-12-08 23:02:06 -0500

    Added channel purger

---
+++ SystemManagementInfrastructure/ChannelPurger.fsx
@@ -0,0 +1,39 @@
+﻿#load "..\References.fsx"
+
+open System
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+
+type Message =
+    | ProcessOrder
+    | PurgeNow
+    | StopPurge
+
+let orderProcessor (mailbox: Actor<_>) =
+    let rec normal () = actor {
+        let! message = mailbox.Receive ()
+        match message with
+        | ProcessOrder -> 
+            printfn "Normal: %A" message
+            return! normal ()
+        | PurgeNow -> 
+            printfn "Normal: %A" message
+            return! purger ()
+        | _ -> return! normal () }
+    and purger () = actor {
+        let! message = mailbox.Receive ()
+        match message with
+        | StopPurge -> 
+            printfn "Purger: %A" message
+            return! normal ()
+        | _ -> return! purger ()
+    }
+    normal ()
+
+let orderProcessorRef = spawn system "orderProcessor" orderProcessor
+
+orderProcessorRef <! ProcessOrder
+orderProcessorRef <! PurgeNow
+orderProcessorRef <! StopPurge
+orderProcessorRef <! ProcessOrder
